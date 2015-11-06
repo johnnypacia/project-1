72983d7c80f64d140d1e6bec3a863fbf93cacfa7
Johnny Pacia <johnny.pacia@gmail.com>
2015-11-06 12:23:08 -0500

    adds eventListeners and first  computer move

--- README.md
+++ README.md
@@ -5,13 +5,21 @@ Simon Says
 User Story: 
 
 -when page loads User should see the board with 4 colors-boxes (not illuminated), a start button, and a score counter set at 0
+
 -When user presses start, computer will randomly light up one of the colors for less than a second (AND may play a custom-built sound??)
+
 -User should be able to click any color-box and in doing so illuminate the box
+
 -If the box clicked matches the box the computer randomly illuminated, move on to next round, increase score counter by 1
+
 -If the box clicked does not match, then user should be notified the game is over
+
 -If player moves on to next round [round 2], computer should light up two boxes in sequence
+
 -If player matches, move on to round 3, where the computer lights up 3 boxes in sequence
+
 -With each round, computer adds and additional box to its sequence of illumination, so in round 10, computer will light up 10 boxes in sequence (each box illuminated for less than half a second)
+
 -score counter keeps track of how many rounds player has completed
 
 Front-end Wireframe: 
--- main.js
+++ main.js
@@ -1,1 +1,40 @@
 console.log("Start it up, lads!")
+
+// document.body.style.backgroundImage = "url('bassmanBackground.jpg')"
+
+var startButton = document.querySelector('#btn');
+var orangeNote = document.querySelector('.orange');
+var greenNote = document.querySelector('.green');
+var purpleNote = document.querySelector('.purple');
+var brownNote = document.querySelector('.brown');  
+
+var lightUp = function (event){
+	if (event.target.id === "btn"){
+		//Computer to randomly select on of the boxes:
+		var blueNotes = [orangeNote, greenNote, purpleNote, brownNote];
+		var randomSelection = blueNotes[Math.floor(Math.random()*blueNotes.length)];
+			randomSelection.classList.add('on');
+			randomSelection.style.backgroundColor = "blue"
+		// console.log(randomSelection);
+		//math.random * Notes.index math.floor
+		//math.floor(Math.random)*blueNotes.length
+	
+	} else if (event.target.classList.contains("orange") === true){
+		console.log("ORANGE LIT");
+	} else if (event.target.classList.contains("green") === true){
+		console.log("GREEN LIT");
+	} else if (event.target.classList.contains("purple") === true){
+		console.log("PURPLE LIT");
+	} else {
+		console.log("BROWN LIT");
+	}
+};
+
+startButton.addEventListener('click', lightUp);
+
+
+
+orangeNote.addEventListener('click', lightUp);
+greenNote.addEventListener('click', lightUp);
+purpleNote.addEventListener('click', lightUp);
+brownNote.addEventListener('click', lightUp);
