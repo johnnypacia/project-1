ec0b79f026815a4d20c6ff49d51adb3a28911db2
Johnny Pacia <johnny.pacia@gmail.com>
2015-11-06 16:01:27 -0500

    creates setTimeout for all eventListeners in JS

--- main.js
+++ main.js
@@ -1,40 +1,63 @@
 console.log("Start it up, lads!")
 
+var startButton;
+var orangeNote;
+var greenNote;
+var purpleNote;
+var brownNote;
+
 // document.body.style.backgroundImage = "url('bassmanBackground.jpg')"
+	var startButton = document.querySelector('#btn');
+	var orangeNote = document.querySelector('.orange');
+	var greenNote = document.querySelector('.green');
+	var purpleNote = document.querySelector('.purple');
+	var brownNote = document.querySelector('.brown');  
 
-var startButton = document.querySelector('#btn');
-var orangeNote = document.querySelector('.orange');
-var greenNote = document.querySelector('.green');
-var purpleNote = document.querySelector('.purple');
-var brownNote = document.querySelector('.brown');  
-
-var lightUp = function (event){
-	if (event.target.id === "btn"){
-		//Computer to randomly select on of the boxes:
-		var blueNotes = [orangeNote, greenNote, purpleNote, brownNote];
-		var randomSelection = blueNotes[Math.floor(Math.random()*blueNotes.length)];
-			randomSelection.classList.add('on');
-			randomSelection.style.backgroundColor = "blue"
-		// console.log(randomSelection);
-		//math.random * Notes.index math.floor
-		//math.floor(Math.random)*blueNotes.length
-	
-	} else if (event.target.classList.contains("orange") === true){
-		console.log("ORANGE LIT");
-	} else if (event.target.classList.contains("green") === true){
-		console.log("GREEN LIT");
-	} else if (event.target.classList.contains("purple") === true){
-		console.log("PURPLE LIT");
-	} else {
-		console.log("BROWN LIT");
-	}
-};
+	var lightUp = function (event){
+		if (event.target.id === "btn"){
+			//Computer to randomly select on of the boxes:
+			var blueNotes = [orangeNote, greenNote, purpleNote, brownNote];
+			var randomSelection = blueNotes[Math.floor(Math.random()*blueNotes.length)];
+				setTimeout(function(){randomSelection.classList.add("black");}, 500);
+				setTimeout(function(){randomSelection.classList.remove("black");}, 1000);
+				// randomSelection.style.backgroundColor = "blue";
+				console.log(randomSelection);
+		} else if (event.target.classList.contains("orange") === true){
+			console.log("ORANGE LIT");
+			setTimeout(function(){
+				orangeNote.classList.add("black");
+			}, 500);
+			setTimeout(function(){
+				orangeNote.classList.remove("black");
+			}, 1000);
+		} else if (event.target.classList.contains("green") === true){
+			console.log("GREEN LIT");
+			setTimeout(function(){
+				greenNote.classList.add("black");
+			}, 500);
+			setTimeout(function(){greenNote.classList.remove("black");}, 1000);
+		} else if (event.target.classList.contains("purple") === true){
+			console.log("PURPLE LIT");
+			setTimeout(function(){
+				purpleNote.classList.add("black");
+			}, 500);
+			setTimeout(function(){
+				purpleNote.classList.remove("black");
+			}, 1000);
+		} else {
+			console.log("BROWN LIT");
+			setTimeout(function(){
+				brownNote.classList.add("black");
+			}, 500);
+			setTimeout(function(){brownNote.classList.remove("black");}, 1000);
+		}
+	};
 
 startButton.addEventListener('click', lightUp);
-
-
-
 orangeNote.addEventListener('click', lightUp);
 greenNote.addEventListener('click', lightUp);
 purpleNote.addEventListener('click', lightUp);
 brownNote.addEventListener('click', lightUp);
+
+
+
