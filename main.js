64363469a7df190ae1bcbbb7ff31c1407cc8f1d6
Johnny Pacia <johnny.pacia@gmail.com>
2015-11-09 18:30:56 -0500

    adds reset and subsequent rounds of play

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ console.log("Start it up, lads!");
 
 
 var startButton = document.querySelector('#btn');
+var resetButton = document.querySelector('#reset');
 var orangeNote = document.querySelector('.orange');
 var greenNote = document.querySelector('.green');
 var purpleNote = document.querySelector('.purple');
@@ -12,47 +13,46 @@ var score = 0;
 
 
 var startItUp = function(event){
-	if (event.target.id === "btn"){
 		//Computer to randomly select one of the boxes:
+		window.setTimeout(gameInstruction, 1000);
 	var blueNotes = [orangeNote, greenNote, purpleNote, brownNote];
 	var randomIndex = Math.floor(Math.random()*blueNotes.length);
 	var randomSelection = blueNotes[randomIndex];
-		if (randomSelection === blueNotes[0]){
-			setTimeout(function(){
-				randomSelection.classList.add("black");
-			}, 500);
-			document.getElementById('orange-note').play();
-			setTimeout(function(){
+	if (randomSelection === blueNotes[0]){
+		setTimeout(function(){
+			randomSelection.classList.add("black");
+		}, 500);
+		document.getElementById('orange-note').play();
+		setTimeout(function(){
+		    randomSelection.classList.remove("black");
+		}, 1000);
+	} else if (randomSelection === blueNotes[1]){
+		setTimeout(function(){
+			randomSelection.classList.add("black");
+		}, 500);
+		document.getElementById('green-note').play();
+		setTimeout(function(){
 			randomSelection.classList.remove("black");
-			}, 1000);
-		} else if (randomSelection === blueNotes[1]){
-			setTimeout(function(){
-				randomSelection.classList.add("black");
-			}, 500);
-			document.getElementById('green-note').play();
-			setTimeout(function(){
+		}, 1000);
+	} else if (randomSelection === blueNotes[2]){
+		setTimeout(function(){
+			randomSelection.classList.add("black");
+		}, 500);
+		document.getElementById('purple-note').play();
+		setTimeout(function(){
 			randomSelection.classList.remove("black");
-			}, 1000);
-		} else if (randomSelection === blueNotes[2]){
-			setTimeout(function(){
-				randomSelection.classList.add("black");
-			}, 500);
-			document.getElementById('purple-note').play();
-			setTimeout(function(){
+		}, 1000);
+	} else {
+		setTimeout(function(){
+			randomSelection.classList.add("black");
+		}, 500);
+		document.getElementById('brown-note').play();
+		setTimeout(function(){
 			randomSelection.classList.remove("black");
-			}, 1000);
-		} else {
-			setTimeout(function(){
-				randomSelection.classList.add("black");
-			}, 500);
-			document.getElementById('brown-note').play();
-			setTimeout(function(){
-			randomSelection.classList.remove("black");
-			}, 1000);
-		}	
+		}, 1000);
+	}	
 	computerMoves.push(randomIndex);
 	console.log(computerMoves);
-	} 
 };
 
 startButton.addEventListener('click', startItUp);
@@ -62,7 +62,7 @@ var lightItUp= function(event){
 		setTimeout(function(){
 			orangeNote.classList.add("black");
 		}, 500);
-			document.getElementById('orange-note').play();
+		document.getElementById('orange-note').play();
 		setTimeout(function(){
 			orangeNote.classList.remove("black");
 		}, 1000);
@@ -72,7 +72,7 @@ var lightItUp= function(event){
 		setTimeout(function(){
 			greenNote.classList.add("black");
 		}, 500);
-			document.getElementById('green-note').play();
+		document.getElementById('green-note').play();
 		setTimeout(function(){
 			greenNote.classList.remove("black");
 		}, 1000);
@@ -82,7 +82,7 @@ var lightItUp= function(event){
 		setTimeout(function(){
 			purpleNote.classList.add("black");
 		}, 500);
-			document.getElementById('purple-note').play();
+		document.getElementById('purple-note').play();
 		setTimeout(function(){
 			purpleNote.classList.remove("black");
 		}, 1000);
@@ -98,6 +98,17 @@ var lightItUp= function(event){
 		}, 1000);
 		playerMoves.push(3);
 		console.log(playerMoves);
+	} 
+	if ( doTheyMatch(computerMoves, playerMoves) ){
+			score ++;
+			window.setTimeout(startItUp, 1500);
+			window.setTimeout(displayScore, 1000);
+		// push another thing into the array
+		// rerun the computer's function
+		//score++;
+	} else {
+		window.setTimeout(playAgain, 1000);
+		resetButton.addEventListener('click', gameReset); 
 	}
 };
 
@@ -111,27 +122,41 @@ brownNote.addEventListener('click', lightItUp);
 // STACK OVERFLOW: 
 // http://stackoverflow.com/questions/22395357/how-to-compare-two-arrays-are-equal-using-javascript-or-jquery
 
+var gameInstruction = function () {
+	document.querySelector('.play-again').textContent='Click the right box to match Simon!';
+};
 
 var displayScore = function (){
-	document.querySelector('.score-counter').textContent='SCORE: ' + score;
+	document.querySelector('.score-counter').textContent='You win!  SCORE: ' + score;
 };
 
 var playAgain = function () {
-	document.querySelector('.play-again').textContent='Press START to play again!';
+	document.querySelector('.play-again').textContent='You lose! press RESET to continue';
 };
 
-var doTheyMatch=function (arr1, arr2){
-	    if (arr1.length !== arr2.length) {
-	    	return false;
-	    }
-	    for (var i = 0, length = arr1.length; i < length; i++){
-	        if (arr1[i] !== arr2[i]){
-	            return false;
-	        }
-	    } return true;
-		};
+var gameReset = function (){
+	computerMoves = [];
+	playerMoves = [];
+	document.querySelector('.play-again').textContent='PRESS start TO PLAY!';
+	document.querySelector('.score-counter').textContent='SCORE: 0';
+};
 
-doTheyMatch(computerMoves, playerMoves);
+var doTheyMatch = function (arr1, arr2){
+	console.log("checking match...");
+    if (arr1.length !== arr2.length) {
+    	return false;
+    }
+    for (var i = 0, length = arr1.length; i < length; i++){
+        if (arr1[i] !== arr2[i]){
+	    	console.log("they don't match");
+            return false;
+        }
+    } 
+    console.log("they match")
+    return true;
+};
+
+
 
 // PHIL'S SUGGESTION: 
 //var isGameActive = true; 
